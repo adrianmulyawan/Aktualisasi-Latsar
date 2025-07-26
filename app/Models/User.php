e1fcf0d9d44c0e3cb1e2a7567746e79ca4b386e8
adrianmulyawan <adrianmulyawan666@gmail.com>
2025-07-26 17:54:43 +0700

    Add Document model, migration, and relationship to User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -51,4 +51,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Category::class);
     }
+
+    public function documents()
+    {
+        return $this->hasMany(Document::class);
+    }
 }
