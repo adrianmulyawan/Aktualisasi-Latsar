1f2490eba820746f534a699d66467189bcac8613
adrianmulyawan <adrianmulyawan666@gmail.com>
2025-07-26 10:56:51 +0700

    Add relation to user and category model

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'name',
+        'slug',
+        'description',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
