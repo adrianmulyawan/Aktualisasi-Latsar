18fa10330d3969bc647b4e63b633c54598ec1893
adrianmulyawan <adrianmulyawan666@gmail.com>
2025-08-30 12:53:42 +0700

    tambah relasi ke revisi dokumen

--- app/Models/User.php
+++ app/Models/User.php
@@ -56,4 +56,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Document::class);
     }
+
+    public function documentRevisions()
+    {
+        return $this->hasMany(DocumentRevision::class);
+    }
 }
